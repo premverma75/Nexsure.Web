f3f9c514d9a6b38f2a0a22300a5f79406f69ab92
premverma75 <verma.prem1993@gmail.com>
2025-04-27 18:25:35 +0530

    Connection setup changes

--- Nexsure.Repositories/DataContext/NexsureAppDbContext.cs
+++ Nexsure.Repositories/DataContext/NexsureAppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace Nexsure.DataBridge.DataContext
 {
-    public class NexsureAppDbContext
+    public class NexsureAppDbContext : DbContext
     {
+        public NexsureAppDbContext(DbContextOptions<NexsureAppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("NexsureConnection");
+            }
+        }
+
+        // Add DbSet properties for your entities here
+        // Example:
+        // public DbSet<YourEntity> YourEntities { get; set; }
     }
 }
